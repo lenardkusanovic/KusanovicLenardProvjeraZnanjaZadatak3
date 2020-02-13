594471c2d4367ba6e5e1b7aa1651911a8f55f5e0
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-02-13 11:34:52 +0100

    Add files via upload

---
+++ KusanovicLenardProvjeraZnanjaZadatak3/KusanovicLenardProvjeraZnanjaZadatak3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KusanovicLenardProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string upis;
+            string x;
+            Console.WriteLine("Upisi rijec: ");
+            upis = Console.ReadLine();
+            for (int i = 0; i < x; i--) {
+                            
+            }
+        }
+    }
+}
